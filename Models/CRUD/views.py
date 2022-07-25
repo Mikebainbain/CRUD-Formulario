8e95335c70745c001072ba8d0a7dfb0340864522
Mikebainbain <mlopezb749@gmail.com>
2022-07-25 12:52:40 -0500

    Creacion de los metodos para el formulario

--- Models/CRUD/views.py
+++ Models/CRUD/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
+from Models.CRUD.forms import formularioEmpleado
+
 # Create your views here.
+class FormularioView(HttpRequest):
+    
+    def index(request):
+        empleado = formularioEmpleado()
+        return render(request, "Empleadoindex.html",{"formulario":empleado})
+    
+    def procesar_formulario(request):
+        empleado = formularioEmpleado()
+        if empleado.is_valid():
+            empleado.save()
+            empleado = formularioEmpleado
+        return render(request, "Empleadoindex.html",{"formulario":empleado, "mesanje": 'OK'})
+     
+
+
